0fa4ecabe410e094adf5df817a73f84cd75deb51
Baisuke <bast.carcamo@duocuc.cl>
2024-10-25 18:28:01 -0300

    aa
    aa

--- app/urls.py
+++ app/urls.py
@@ -1,20 +1,21 @@
 from django.urls import path
-from .views import agregar_producto, confirmar_pedido, eliminar_producto, home, limpiar_carrito, listar_pedidos, mis_pedidos, pagar_pedido, rechazar_pedido, restar_producto, pago, exit, register,realizar_pedido
-
+from .views import agregar_producto, confirmar_pedido, eliminar_producto, home, limpiar_carrito, listar_pedidos, mis_pedidos, pagar_pedido, pago_exitoso, perfil_usuario, rechazar_pedido, restar_producto, pago, exit, register, realizar_pedido
 
 urlpatterns = [
     path('', home, name="home"),
-    path('agregar/<int:producto_id>/',agregar_producto, name="add"),
-    path('eliminar/<int:producto_id>/',eliminar_producto, name="del"),
-    path('restar/<int:producto_id>/',restar_producto, name="sub"),
-    path('limpiar/',limpiar_carrito, name="cls"),
+    path('agregar/<int:producto_id>/', agregar_producto, name="add"),
+    path('eliminar/<int:producto_id>/', eliminar_producto, name="del"),
+    path('restar/<int:producto_id>/', restar_producto, name="sub"),
+    path('limpiar/', limpiar_carrito, name="cls"),
     path('pago/', pago, name='pago'),
-    path('logout/',exit, name='exit'),
-    path('register/',register, name='register'),
+    path('logout/', exit, name='exit'),
+    path('register/', register, name='register'),
     path('realizar-pedido/', realizar_pedido, name='realizar_pedido'),
     path('pedidos/', listar_pedidos, name='listar_pedidos'),
     path('pedidos/confirmar/<int:pedido_id>/', confirmar_pedido, name='confirmar_pedido'),
     path('pedidos/rechazar/<int:pedido_id>/', rechazar_pedido, name='rechazar_pedido'),
     path('mis_pedidos/', mis_pedidos, name='mis_pedidos'),
     path('pagar_pedido/<int:pedido_id>/', pagar_pedido, name='pagar_pedido'),
+    path('perfil/', perfil_usuario, name='perfil_usuario'),
+    path('pago_exitoso/', pago_exitoso, name='pago_exitoso'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from django.http import HttpResponse
+from http.client import HTTPResponse
 from django.shortcuts import render, redirect ,  get_object_or_404
 from app.carrito import Carrito
 from .models import Producto, Pedidos, ProductoPedido
@@ -7,6 +10,13 @@ from django.contrib.auth import logout, authenticate, login
 from .forms import CustomUserCreationForm
 from django.contrib import messages
 from django.core.paginator import Paginator
+import bcchapi
+from django.shortcuts import render
+from .models import Pedidos
+from django.shortcuts import render, redirect
+from .models import Pedidos
+from transbank.webpay.webpay_plus.transaction import Transaction
+
 
 def home(request):
     busqueda = request.GET.get("Buscar") 
@@ -39,7 +49,7 @@ def pago(request):
     carrito = Carrito(request)
     total = 0
 
-    # Crear una lista para almacenar los productos del carrito
+
     productos_del_carrito = []
 
     for item in carrito:
@@ -147,14 +157,48 @@ def realizar_pedido(request):
 
 @login_required
 def mis_pedidos(request):
-    # Obtener todos los pedidos del usuario actual
     pedidos = Pedidos.objects.filter(cliente=request.user)
-    
+    siete = bcchapi.Siete(file="credenciales.txt")
+    fecha_actual = datetime.now().strftime("%Y-%m-%d")
+
+    series_monedas = {
+        "dolar": "F073.TCO.PRE.Z.D",
+        "euro": "F072.CLP.EUR.N.O.D", 
+        "libra": "F072.CLP.GBP.N.O.D",  
+        "yen": "F072.CLP.JPY.N.O.D" 
+    }
+
+    valores_monedas = {}
+
+
+    for moneda, serie in series_monedas.items():
+        try:
+            retcuadro = siete.cuadro(series=[serie], nombres=[moneda], desde=fecha_actual, hasta=fecha_actual)
+            valor_moneda = retcuadro.iloc[0, 0]
+            print(f"Valor del {moneda}: {valor_moneda}")
+
+            if valor_moneda is None or valor_moneda <= 0:
+                raise ValueError(f"El valor de {moneda} no es válido.")
+            
+            valores_monedas[moneda] = valor_moneda
+        except Exception as e:
+            print(f"Error al obtener el valor de {moneda}: {e}")
+            valores_monedas[moneda] = 1 
+
     for pedido in pedidos:
-        pedido.productos = ProductoPedido.objects.filter(pedido=pedido)
+        pedido.total_en_dolares = pedido.total / valores_monedas["dolar"]
+        pedido.total_en_euros = pedido.total / valores_monedas["euro"]
+        pedido.total_en_libras = pedido.total / valores_monedas["libra"]
+        pedido.total_en_yenes = pedido.total / valores_monedas["yen"]
 
-    return render(request, 'app/mis_pedidos.html', {'pedidos': pedidos})
+    moneda_seleccionada = request.POST.get('moneda', 'dolar') 
+    if moneda_seleccionada in valores_monedas:
+        for pedido in pedidos:
+            total_convertido = pedido.total / valores_monedas[moneda_seleccionada]
+            pedido.total_convertido = total_convertido
+            pedido.moneda_seleccionada = moneda_seleccionada
 
+    return render(request, 'app/mis_pedidos.html', {'pedidos': pedidos, 'valores_monedas': valores_monedas})
 
 
 
@@ -172,14 +216,14 @@ def listar_pedidos(request):
 @permission_required('app.change_pedidos')
 def confirmar_pedido(request, pedido_id):
     pedido = get_object_or_404(Pedidos, id=pedido_id)
-    pedido.estado = 'aceptado'  # Actualiza el estado a aceptado
+    pedido.estado = 'aceptado' 
     pedido.save()
     return redirect('listar_pedidos')
 
 @permission_required('app.change_pedidos')
 def rechazar_pedido(request, pedido_id):
     pedido = get_object_or_404(Pedidos, id=pedido_id)
-    pedido.estado = 'rechazado'  # Actualiza el estado a rechazado
+    pedido.estado = 'rechazado'
     pedido.save()
     return redirect('listar_pedidos')
 
@@ -189,3 +233,58 @@ def pagar_pedido(request, pedido_id):
     
 
     return render(request, 'app/pagar_pedido.html', {'pedido': pedido})
+
+@login_required
+def perfil_usuario(request):
+    user = request.user
+    if not user.is_staff:
+        pedidos_aceptados = Pedidos.objects.filter(cliente=user, estado='aceptado').count()
+        pedidos_pagados = Pedidos.objects.filter(cliente=user, estado='pagado').count()
+        
+        context = {
+            'nombre': user.get_full_name(),
+            'correo': user.email,
+            'pedidos_aceptados': pedidos_aceptados,
+            'pedidos_pagados': pedidos_pagados,
+            'es_staff': False
+        }
+    else:
+        context = {
+            'nombre': user.get_full_name(),
+            'correo': user.email,
+            'rol': user.groups.first().name if user.groups.exists() else 'Sin rol asignado',
+            'es_staff': True
+        }
+    
+    return render(request, 'app/perfil_usuario.html', context)
+
+def pagar_pedido(request, pedido_id):
+
+    pedido = get_object_or_404(Pedidos, id=pedido_id)
+
+    buy_order = str(pedido.id) 
+    session_id = "sesion_456" 
+    amount = pedido.total 
+    return_url = request.build_absolute_uri('app/pago_exitoso/') 
+
+    try:
+        # Crear la transacción
+        transaction = Transaction.create(
+            buy_order=buy_order,
+            session_id=session_id,
+            amount=amount,
+            return_url=return_url
+        )
+        return redirect(transaction.url)  # Redirigir al usuario a la URL de pago de Transbank
+    except Exception as e:
+        # Manejar el error adecuadamente
+        print(f"Error al crear la transacción: {e}")
+        return HttpResponse("Hubo un error al procesar el pago.")
+    
+def pago_exitoso(request):
+    return render(request, 'app/pago_exitoso.html')  # Crea una plantilla para mostrar el mensaje de éxito
+
+
+
+
+
